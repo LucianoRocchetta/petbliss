690060aaa9aee41249c0b4ce744f7aa8fd886486
LucianoRocchetta <lucianorocchetta@gmail.com>
2025-04-21 17:44:36 -0300

    Add: Adding Petbliss favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,12 +5,21 @@ import { TopbarMenu } from "@/components/shared";
 import { Roboto } from "next/font/google";
 import { SessionProvider } from "next-auth/react";
 import { Footer } from "@/containers/footer";
+import { Metadata } from "next";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["400", "500", "700"],
   style: ["normal", "italic"],
 });
+
+const metadata: Metadata = {
+  title: {
+    default: "Petbliss",
+    absolute: "",
+    template: "",
+  },
+};
 
 export default function RootLayout({
   children,
